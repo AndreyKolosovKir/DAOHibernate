db2c7fb7db76ce75504cd08e7733f4775682d699
AndreyKolosovKir <andrey.e.04@gmail.com>
2023-11-09 18:31:33 +0300

    new commit.add annotation-security  in controller and update config

--- src/main/java/com/example/daohibernate/controller/ControllerUserDate.java
+++ src/main/java/com/example/daohibernate/controller/ControllerUserDate.java
@@ -3,8 +3,12 @@ package com.example.daohibernate.controller;
 import com.example.daohibernate.date.Persons;
 import com.example.daohibernate.service.ServiceUser;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +22,7 @@ public class ControllerUserDate {
     }
 
     //Данный запрос поиска по городу оставил без изменений (так же в репозитории)
+    @RolesAllowed({"ROLE_WRITE"})
     @GetMapping("/persons/by-city")
     @ResponseStatus(code = HttpStatus.OK)
     public List<Persons> getMapping(@RequestParam("city") String city) {
@@ -25,13 +30,15 @@ public class ControllerUserDate {
     }
 
     //Добавление нового пользователя
-    @PostMapping("/persons")
+    @RolesAllowed({"ROLE_WRITE"})
+    @PostMapping("/persons/new_person")
     @ResponseStatus(code = HttpStatus.OK)
     public Object savePerson(@RequestBody Persons person) {
         return serviceUser.savePersons(person);
     }
 
     //Поиск пользователей по id
+    @Secured({"ROLE_READ"})
     @GetMapping("/persons/by-id")
     @ResponseStatus(code = HttpStatus.OK)
     public Object findById(@RequestParam("id") int id) {
@@ -39,6 +46,8 @@ public class ControllerUserDate {
     }
 
     //Вывод всех пользователей
+    //Принимает в качестве параметра имя пользователя
+    @PreAuthorize("authentication.principal.username == 'Vadim'")
     @GetMapping("/persons")
     @ResponseStatus(code = HttpStatus.OK)
     public Object findAll() {
@@ -46,6 +55,7 @@ public class ControllerUserDate {
     }
 
     //Обновление номера телефона по id
+    @RolesAllowed({"ROLE_WRITE"})
     @PostMapping("/persons/update-phone-by-id")
     @ResponseStatus(code = HttpStatus.OK)
     public Object updatePhoneNumber(@RequestBody Persons persons) {
@@ -55,6 +65,7 @@ public class ControllerUserDate {
     }
 
     //Поиск по году
+    @RolesAllowed({"ROLE_WRITE"})
     @GetMapping("/persons/by-age-less")
     @ResponseStatus(code = HttpStatus.OK)
     public List<Persons> getPersonsByAgeLess(@RequestParam("age") Integer age) {
@@ -62,6 +73,7 @@ public class ControllerUserDate {
     }
 
     //Поиск по имени и фамилии
+    @Secured({"ROLE_READ"})
     @GetMapping("/persons/by-name-and-surname")
     @ResponseStatus(code = HttpStatus.OK)
     public Optional<Persons> getPersonsByNameAndSurname(
@@ -72,6 +84,7 @@ public class ControllerUserDate {
     }
 
     //Удаление по id
+    @PostAuthorize("hasAnyRole('WRITE') or hasAnyRole('DELETE')")
     @DeleteMapping("/persons/delete-by-id")
     @ResponseStatus(code = HttpStatus.OK)
     public Object delete(@RequestParam("id") int id) {
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,23 +1,23 @@
 create schema netology;
-
-CREATE TABLE netology.persons
-(
-    Name        varchar(60),
-    Surname     varchar(60),
-    Age         varchar(3),
-    PhoneNumber varchar(12),
-    City        varchar(50),
-    PRIMARY KEY (Name, Surname, Age)
-);
-
-insert into netology.persons
-values ('Viktor', 'Perminov', '25', '89658567485', 'Moscow');
+--
+-- CREATE TABLE netology.persons
+-- (
+--     Name        varchar(60),
+--     Surname     varchar(60),
+--     Age         varchar(3),
+--     PhoneNumber varchar(12),
+--     City        varchar(50),
+--     PRIMARY KEY (Name, Surname, Age)
+-- );
 
 insert into netology.persons
-values ('Natalia', 'Trifonova', '27', '89658567485', 'Kirov');
+values (1, 'Viktor', 'Perminov', 25, '89658567485', 'Moscow');
 
 insert into netology.persons
-values ('Anton', 'Vachevske', '28', '89658567485', 'Vladivostok');
+values (2, 'Natalia', 'Trifonova', 27, '89658567485', 'Kirov');
 
 insert into netology.persons
-values ('Andrey', 'Suvorov', '30', '89658567485', 'Moscow');
+values (3, 'Anton', 'Vachevske', 28, '89658567485', 'Vladivostok');
+
+insert into netology.persons
+values (4, 'Andrey', 'Suvorov', 30, '89658567485', 'Moscow');
